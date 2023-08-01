0dd280b802fb5ea0ed8aeba75d87522fad4f8430
Bondanrey <53423209+bondan1@users.noreply.github.com>
2023-08-01 19:27:35 +0700

    Source-Code-Landing-Page
    Signed-off-by: Bondanrey <53423209+bondan1@users.noreply.github.com>

---
+++ underconstarction/js/slick-qarousel.js
@@ -0,0 +1,11 @@
+<script>
+    // Inisialisasi Slick Carousel
+    $(document).ready(function(){
+        $('#carousel').slick({
+            infinite: true, // Putar terus menerus
+            slidesToShow: 1, // Tampilkan 1 slide per view
+            slidesToScroll: 1 // Geser 1 slide pada setiap perpindahan
+            // Tambahkan opsi lain sesuai kebutuhan
+        });
+    });
+</script>
