61b23eb965465aef851b8f3d7ca8ca9e3d2a41a5
Jerryco Sompotan <70189950+nexzuin@users.noreply.github.com>
2023-08-03 11:42:06 +0700

    Update slider-carousel.js
    Signed-off-by: Jerryco Sompotan <70189950+nexzuin@users.noreply.github.com>

--- underconstarction/js/slider-carousel.js
+++ underconstarction/js/slider-carousel.js
@@ -1,16 +1,6 @@
-// HARDCODE
-
-// const slides = [
-//   {
-//     title: "Company Profile",
-//     subtitle: "Warung Pintar",
-//     image: "images/slider/image1.png"
-//   },
-// ]
-
 var swiper = new Swiper("#carousel", {
   slidesPerView: 1,
-  spaceBetween: 30,
+  spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -19,10 +9,10 @@ var swiper = new Swiper("#carousel", {
     480: {
       slidesPerView: 1, // 1 slides
     },
-    600: {
+    800: {
       slidesPerView: slides.length > 1 ? 2 : 1, // 2 slides
     },
-    800: {
+    1000: {
       slidesPerView: slides.length > 2 ? 3 : (slides.length > 1 ? 2 : 1),  // 3 slides
     },
   }
@@ -35,20 +25,23 @@ function appendSlide() {
     // CREATE ELEMENT
     const slide = document.createElement('div')
     const content = document.createElement('div')
-    const image = document.createElement('img')
+    const imageContainer = document.createElement('div')
+    const image = document.createElement('div')
     const title = document.createElement('h2')
     const subtitle = document.createElement('p')
 
     // ADD ATTRIBUTES TO ELEMENT
     slide.className = "swiper-slide"
     content.className = "swiper-content"
-    image.src = item.image
-    image.alt = item.title
+    image.className = "swiper-img"
+    image.style.backgroundImage = `url('${item.image}')`
+    imageContainer.className = "swiper-img-container"
     title.innerText = item.title
     subtitle.innerText = item.subtitle
 
     // APPEND CHILD ELEMENT
-    content.appendChild(image)
+    imageContainer.appendChild(image)
+    content.appendChild(imageContainer)
     content.appendChild(title)
     content.appendChild(subtitle)
     slide.append(content)
